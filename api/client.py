7bf5c08e2492301253957c532ebcf3ef62d67b53
AlinaBlahun <ablahun@cisco.com>
2020-06-23 12:16:30 +0300

    CCTRI-1214: Refactor.

--- api/client.py
+++ api/client.py
@@ -2,17 +2,21 @@ from http import HTTPStatus
 
 import requests
 
-from api.errors import (
-    CriticalSecurityTrailsResponseError
-)
+from api.errors import CriticalSecurityTrailsResponseError
 from api.utils import join_url
 
 NOT_CRITICAL_ERRORS = (
     HTTPStatus.BAD_REQUEST, HTTPStatus.NOT_FOUND, HTTPStatus.NOT_ACCEPTABLE
 )
 
+IP = 'ip'
+IPV6 = 'ipv6'
+DOMAIN = 'domain'
+
 
 class SecurityTrailsClient:
+    OBSERVABLE_TO_FILTER_TYPE = {IP: 'ipv4', IPV6: 'ipv6'}
+
     def __init__(self, base_url, api_key, user_agent, number_of_pages=1):
         self.base_url = base_url
         self.headers = {
@@ -22,27 +26,24 @@ class SecurityTrailsClient:
         }
         self.number_of_pages = number_of_pages
 
-    def _request(self, path, method='GET', body=None, page=1):
-        params = {'page': page}
-        url = join_url(self.base_url, path)
-
-        response = requests.request(
-            method, url, headers=self.headers, json=body, params=params
-        )
-
-        if response.ok:
-            return response.json()
-
-        if response.status_code in NOT_CRITICAL_ERRORS:
-            return []
-
-        raise CriticalSecurityTrailsResponseError(response)
-
     def ping(self):
         """
         https://docs.securitytrails.com/reference#ping
         """
         return self._request('ping')
+
+    def get_data(self, observable):
+        observable_type = observable['type']
+
+        if observable_type == DOMAIN:
+            return (self._get_pages(observable, self._history, type_='a'),
+                    self._get_pages(observable, self._history, type_='aaaa'))
+
+        if observable_type in (IP, IPV6):
+            return self._get_pages(observable, self._domain_list)
+
+        else:
+            return {}
 
     def _history(self, observable, type_, page=1):
         """
@@ -59,7 +60,7 @@ class SecurityTrailsClient:
         https://docs.securitytrails.com/reference#domain-search
         """
 
-        filter_type = {'ip': 'ipv4', 'ipv6': 'ipv6'}.get(observable['type'])
+        filter_type = self.OBSERVABLE_TO_FILTER_TYPE.get(observable['type'])
 
         body = {
             "filter": {
@@ -68,21 +69,20 @@ class SecurityTrailsClient:
         }
         return self._request('domains/list', 'POST', body, page=page)
 
-    @staticmethod
-    def _fetch_max_possible_page(data):
-        return (data.get('pages')
-                or data.get('meta', {}).get('max_page')
-                or 0)
-
     def _need_all_pages(self):
         return self.number_of_pages == 0
 
     def _get_pages(self, observable, endpoint, *args, **kwargs):
+        def max_possible_page(data):
+            return (data.get('pages')
+                    or data.get('meta', {}).get('max_page')
+                    or 0)
+
         data = endpoint(observable, *args, **kwargs)
 
         if data and self.number_of_pages != 1:
 
-            max_page = self._fetch_max_possible_page(data)
+            max_page = max_possible_page(data)
             if self.number_of_pages < max_page and not self._need_all_pages():
                 max_page = self.number_of_pages
 
@@ -95,15 +95,18 @@ class SecurityTrailsClient:
 
         return data
 
-    def get_data(self, observable):
-        observable_type = observable['type']
+    def _request(self, path, method='GET', body=None, page=1):
+        params = {'page': page}
+        url = join_url(self.base_url, path)
 
-        if observable_type == 'domain':
-            return (self._get_pages(observable, self._history, type_='a'),
-                    self._get_pages(observable, self._history, type_='aaaa'))
+        response = requests.request(
+            method, url, headers=self.headers, json=body, params=params
+        )
 
-        if observable_type in ('ip', 'ipv6'):
-            return self._get_pages(observable, self._domain_list)
+        if response.ok:
+            return response.json()
 
-        else:
-            return {}
+        if response.status_code in NOT_CRITICAL_ERRORS:
+            return []
+
+        raise CriticalSecurityTrailsResponseError(response)
